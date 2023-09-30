cd8ac4c201068f55b2d25c7f69c241950dfa08f7
GomidesTs <gomidestulio5@gmail.com>
2023-09-29 23:45:34 -0300

    feat: tabs
    We started styling the page, with the content building the tabs, using the @radix/react-tabs package

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { SettingsTabs } from '@/components/SettingsTabs';
+
 export default function Home() {
   return (
-    <section>
-      <h1>Hello Word</h1>
-    </section>
+    <>
+      <h1 className="to-zinc-900 text-3xl font-medium">Settings</h1>
+
+      <SettingsTabs />
+    </>
   );
 }
