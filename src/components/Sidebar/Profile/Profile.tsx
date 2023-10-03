c53bc58d7f19757a96ccec65e21b8bba31bedb0f
GomidesTs <gomidestulio5@gmail.com>
2023-10-03 12:35:39 -0300

    style: button
    Created new button variant called 'ghost'. This variant will be smaller and have a less rounded shape

--- src/components/Sidebar/Profile/Profile.tsx
+++ src/components/Sidebar/Profile/Profile.tsx
@@ -1,6 +1,8 @@
 import { LogOut } from 'lucide-react';
 import Image from 'next/image';
 
+import { Button } from '@/components/Button';
+
 export const Profile = () => {
   return (
     <div className="grid grid-cols-profile items-center gap-3">
@@ -20,9 +22,9 @@ export const Profile = () => {
           gomidests@gomidesweb.com.br
         </span>
       </div>
-      <button type="button" className="ml-auto rounded-md p-2 hover:bg-zinc-50">
+      <Button type="button" variant="ghost">
         <LogOut className="h-5 w-5 text-zinc-500" />
-      </button>
+      </Button>
     </div>
   );
 };
