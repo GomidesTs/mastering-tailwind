8f0180cf13c374983eea6aebb26d153c920b9cc3
GomidesTs <gomidestulio5@gmail.com>
2023-10-03 00:36:43 -0300

    style: select
    Created animation and keyframe settings to determine how animations work. We created the animation, SlideDownAndFade, to control the opening of a Select

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,7 +16,16 @@ const config: Config = {
       borderWidth: {
         6: '6px'
       },
+      keyframes: {
+        slideDownAndFade: {
+          from: { opacity: 0, transform: 'translateY(-2px)' },
+          to: { opacity: 1, transform: 'translateY(0)' }
+        }
+      },
 
+      animation: {
+        slideDownAndFade: 'slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)'
+      },
       colors: {
         violet: {
           25: '#fcfaff'
